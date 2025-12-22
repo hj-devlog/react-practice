dc1650b26f75688bb24e306cfb6fb568addadae5
hj <m_i_n_imind@naver.com>
2025-12-22 18:10:24 +0900

    React 함수형 컨포넌트 props 실습

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,36 +2,54 @@ import { Component, useState } from 'react';
 import './App.css';
 
 
-// function App() {
- //const [counter, setCounter]= useState(1)
-  //return (
-  // <>
-  // <div>counter : {counter} </div>
-  // <button 
-  // onClick={() => {setCounter((prev) => prev +1)}}> +
-  // </button>
-  // <button
-  // onClick={() => {setCounter((prev) => prev -1)}}> -
-  // </button>
-  // </>
-// 
-  // );
+function App() {
+const [counter, setCounter]= useState(1)
+  return (
+<>
+<Count 
+counter ={counter} 
+hello = "hello" 
+array = {[1, 2, 3, "안녕하세요"]}
+/> 
+<button
+onClick={() => {
+  setCounter((prev) => prev +1)
+  }}
+  > 
+    +
+</button>
+<button
+onClick={() => {
+  setCounter((prev) => prev -1)
+  }}
+  >
+    -
+</button>
+</>
+);
+}
+
+// class App extends Component {
+//   state = {counter: 2};
+//   render(){
+//     return(
+//       <>
+//       <div>counter {this.state.counter}</div>
+//       <button 
+//       onClick={() => this.setState({counter: this.state.counter +1})}
+//       >+</button>
+//       <button onClick={() => this.setState({counter: this.state.counter -1})}
+//       >-</button>
+//       </>
+//     );
+//   }
 // }
 
-class App extends Component {
-  state = {counter: 2};
-  render(){
-    return(
-      <>
-      <div>counter {this.state.counter}</div>
-      <button 
-      onClick={() => this.setState({counter: this.state.counter +1})}
-      >+</button>
-      <button onClick={() => this.setState({counter: this.state.counter -1})}
-      >-</button>
-      </>
-    );
-  }
+function Count ({array, counter, hello}) {
+  console.log("array", array);
+  console.log("counter", counter);
+  console.log("hello", hello);
+  return <div>counter : {counter}</div>
 }
 
 export default App;
