0e6aa58ef38a2aaf2160132a1ef5893cd93b38a9
hj <m_i_n_imind@naver.com>
2025-12-22 17:40:24 +0900

    React State 실습

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,31 +1,37 @@
+import { Component, useState } from 'react';
 import './App.css';
 
-const fruits = [
-    {name: "딸기", color: "빨강", 크기: 2 },
-    {name: "바나나", color: "노랑", 크기: 5 },
-    {name: "키위", color: "초록", 크기: 3 },
-    {name: "사과", color: "빨강", 크기: 4 },
-    {name: "블루베리", color: "파랑", 크기: 1 },
-    {name: "오렌지", color: "주황", 크기: 4 },
-    {name: "포도", color: "보라", 크기: 2 },
-    {name: "레몬", color: "노랑", 크기: 3 },
-    {name: "체리", color: "빨강", 크기: 2 },
-    {name: "수박", color: "초록", 크기: 8 },
-];
 
-function App() {
-  return (
-    <>
-    {fruits.
-    filter((fruit) => fruit.color === "노랑")
-    .map((fruit) => (
-      <div key={fruit.name}>
-      이름 : {fruit.name}, 색깔 : {fruit.color},
-      크기 : {fruit.크기}
-    </div>
-    ))}
-    </>
-  );
+// function App() {
+ //const [counter, setCounter]= useState(1)
+  //return (
+  // <>
+  // <div>counter : {counter} </div>
+  // <button 
+  // onClick={() => {setCounter((prev) => prev +1)}}> +
+  // </button>
+  // <button
+  // onClick={() => {setCounter((prev) => prev -1)}}> -
+  // </button>
+  // </>
+// 
+  // );
+// }
+
+class App extends Component {
+  state = {counter: 2};
+  render(){
+    return(
+      <>
+      <div>counter {this.state.counter}</div>
+      <button 
+      onClick={() => this.setState({counter: this.state.counter +1})}
+      >+</button>
+      <button onClick={() => this.setState({counter: this.state.counter -1})}
+      >-</button>
+      </>
+    );
+  }
 }
 
 export default App;
